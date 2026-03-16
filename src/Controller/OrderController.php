a1cfe9dbd6798aeb06cb46b5232c54d874d7fb36
Thomas Merlin <42900754+thomasmerlin@users.noreply.github.com>
2026-03-16 14:36:37 +0000

    Test

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class OrderController extends AbstractController
 {
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,7 +8,7 @@ use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class ProductController extends AbstractController
 {
